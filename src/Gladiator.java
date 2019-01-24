6708b9fb920e098841965631fe266216a69ee952
John <jmorgan@basecampcodingacademy.org>
2019-01-24 10:12:09 -0600

    Removed getter and setters for which they are unnecessary for this project.

--- src/Gladiator.java
+++ src/Gladiator.java
@@ -1,13 +1,13 @@
 //Default Gladiator
 abstract class Gladiator {
-    private int health;
-    private int level;
-    private int mana;
-    private int manaCost;
-    private int defense;
-    private int atkLow;
-    private int atkHigh;
-    private String spellName;
+    public int health;
+    public int level;
+    public int mana;
+    public int manaCost;
+    public int defense;
+    public int atkLow;
+    public int atkHigh;
+    public String spellName;
 
     Gladiator(){
         this.health = 100;
@@ -25,79 +25,16 @@ abstract class Gladiator {
     abstract void levelUp();
 
     public int attackDamage(){
-        return Utils.randRange(this.getAtkHigh(), this.getAtkLow());
+        return Utils.randRange(atkHigh, atkLow);
     }
 
     public boolean isDead(){
-        if (this.getHealth() <= 0) {
-            this.setHealth(0);
+        if (health <= 0) {
+            health = 0;
             return true;
         } else {
             return false;
         }
     }
 
-    public int getAtkHigh() {
-        return atkHigh;
-    }
-
-    public void setAtkHigh(int atkHigh) {
-        this.atkHigh = atkHigh;
-    }
-
-    public int getAtkLow() {
-        return atkLow;
-    }
-
-    public void setAtkLow(int atkLow) {
-        this.atkLow = atkLow;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public int getMana() {
-        return mana;
-    }
-
-    public void setMana(int mana) {
-        this.mana = mana;
-    }
-
-    public int getDefense() {
-        return defense;
-    }
-
-    public void setDefense(int defense) {
-        this.defense = defense;
-    }
-
-    public String getSpellName() {
-        return spellName;
-    }
-
-    public void setSpellName(String spellName) {
-        this.spellName = spellName;
-    }
-
-    public int getManaCost() {
-        return manaCost;
-    }
-
-    public void setManaCost(int manaCost) {
-        this.manaCost = manaCost;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
 }
--- src/Warrior.java
+++ src/Warrior.java
@@ -1,22 +1,22 @@
 public class Warrior extends Gladiator{
     public Warrior() {
-        this.setMana(40);
-        this.setManaCost(30);
-        this.setDefense(25);
-        this.setAtkHigh(20);
-        this.setAtkLow(20);
-        this.setSpellName("Full Power Smash");
+        this.mana = 40;
+        this.manaCost= 30;
+        this.defense = 25;
+        this.atkHigh= 20;
+        this.atkLow= 10;
+        this.spellName = "Full Power Smash";
     }
 
     @Override
     public void castSpell(Gladiator enemy) {
-        enemy.setHealth(enemy.getHealth()-this.getAtkHigh());
-        enemy.setMana(enemy.getMana()-enemy.getManaCost());
+        enemy.health -= this.atkHigh;
+        enemy.mana -= enemy.manaCost;
     }
 
     @Override
     void levelUp() {
-
+        this.health += 10;
     }
 
 
