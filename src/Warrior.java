ece5ccccee6caae8d35e845461b29e288a117a17
John <jmorgan@basecampcodingacademy.org>
2019-01-24 09:54:09 -0600

    Started building the Warrior subclass to make a gladiator.

---
+++ src/Warrior.java
@@ -0,0 +1,16 @@
+public class Warrior extends Gladiator{
+    public Warrior() {
+        this.setMana(40);
+        this.setManaCost(30);
+        this.setDefense(25);
+        this.setAtkHigh(20);
+        this.setAtkLow(20);
+        this.setSpellName("Full Power Smash");
+    }
+
+    @Override
+    public void castSpell(Gladiator enemy) {
+        enemy.setHealth(enemy.getHealth()-this.getAtkHigh());
+        enemy.setMana(enemy.getMana()-enemy.getManaCost());
+    }
+}
