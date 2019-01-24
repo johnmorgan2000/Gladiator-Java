473f25f1dc1c024ed28b7a28a203fe2465ba50e4
John <jmorgan@basecampcodingacademy.org>
2019-01-24 09:08:25 -0600

    Created attackDamage to get the Gladiator's attack range.

--- src/Gladiator.java
+++ src/Gladiator.java
@@ -1,12 +1,46 @@
+//Default Gladiator
 abstract class Gladiator {
     private int health;
     private int mana;
     private int defense;
+    private int atkLow;
+    private int atkHigh;
 
     Gladiator(){
         this.health = 100;
         this.mana = 100;
         this.defense = 100;
+        this.atkHigh = 20;
+        this.atkLow = 5;
+    }
+
+    public int attackDamage(){
+        return Utils.randRange(this.getAtkHigh(), this.getAtkLow());
+    }
+
+    public boolean isDead(){
+        if (this.getHealth() <= 0) {
+            this.setHealth(0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getAtkHigh() {
+        return atkHigh;
+    }
+
+    public void setAtkHigh(int atkHigh) {
+        this.atkHigh = atkHigh;
+    }
+
+    public int getAtkLow() {
+        return atkLow;
+    }
+
+    public void setAtkLow(int atkLow) {
+        this.atkLow = atkLow;
     }
 
     public int getHealth() {
@@ -33,13 +67,5 @@ abstract class Gladiator {
         this.defense = defense;
     }
 
-    public boolean isDead(){
-        if (this.getHealth() <= 0) {
-            this.setHealth(0);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
+
 }
