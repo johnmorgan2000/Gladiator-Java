6ca344fcb4b890549189b2d06a74fad7f63e5d67
John <jmorgan@basecampcodingacademy.org>
2019-01-24 15:29:37 -0600

    Enemy now contains a small bot for making battle choices. Could possibly be moved or changed later.

--- src/Enemy.java
+++ src/Enemy.java
@@ -12,6 +12,18 @@ abstract class Enemy {
         return gladiator;
     }
 
+    public static void botChoice(Gladiator enemy, Gladiator player){
+        if (player.health <= enemy.atkLow){
+            enemy.attack(player);
+        } else if (enemy.mana >= enemy.manaCost){
+            enemy.castSpell(player);
+        }else if (enemy.mana + enemy.manaRefillRate >= enemy.manaCost ){
+            enemy.regenerate();
+        }else{
+            enemy.attack(player);
+        }
+    }
+
     public static void getToLevel(Gladiator g, int level){
         for (int i=0; i<=level; i++){
             g.levelUp();
