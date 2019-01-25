b8ef77be2b15418dffb78424a6209dc2339b7591
John <jmorgan@basecampcodingacademy.org>
2019-01-25 11:24:28 -0600

    Created flipCoin method to be used for deciding on who goes first.

--- src/Utils.java
+++ src/Utils.java
@@ -5,4 +5,19 @@ public class Utils {
         Random rng = new Random();
         return rng.nextInt(high)+low;
     }
+
+    public static void main(String[] args) {
+        System.out.println(flipCoin());
+    }
+
+
+    static boolean flipCoin(){
+        switch(randRange(2,1)){
+            case 2:
+                return true;
+            case 1:
+                return false;
+        }
+        return false;
+    }
 }
