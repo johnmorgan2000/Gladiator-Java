1b0b100d670fd5b85617c9a408af6371fffa5ac7
John <jmorgan@basecampcodingacademy.org>
2019-01-28 09:55:57 -0600

    Improved and fixed some gameplay visuals and bugs.

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program {
@@ -12,11 +13,14 @@ public class Program {
 
     public static void beginGame(Gladiator player, Scanner reader){
         int round = 1;
+        ArrayList<String> battleLog = new ArrayList<>();
+
         while (!player.isDead()) {
-            Gladiator enemy = Enemy.makeEnemy("Joe", 2, 1);
+            Gladiator enemy = Enemy.generateNextEnemy(player, round);
             player.regenerate();
             enemy.regenerate();
-            battle(player, reader, enemy);
+            enemy.health -= 50;
+            battle(player, reader, enemy, battleLog);
             round++;
             player.levelUp();
         }
@@ -24,56 +28,82 @@ public class Program {
         System.exit(0);
     }
 
-    public static void battle(Gladiator player, Scanner reader, Gladiator enemy){
+    public static void battle(Gladiator player, Scanner reader, Gladiator enemy, ArrayList<String> battleLog){
         boolean playerAttackFirst = Utils.flipCoin();
         if (playerAttackFirst){
             while (!player.isDead() && !enemy.isDead()) {
-                displayOpponets(player, enemy);
-                playerBattleChoice(player, reader, enemy);
-                Enemy.botChoice(enemy, player);
+                Utils.clearConsole();
+                displayOpponents(player, enemy);
+                playerBattleChoice(player, reader, enemy,  battleLog);
+                Enemy.botChoice(enemy, player,  battleLog);
             }
         }else {
             while (!player.isDead() && !enemy.isDead()) {
-                displayOpponets(player, enemy);
-                Enemy.botChoice(enemy, player);
-                playerBattleChoice(player, reader, enemy);
+                Utils.clearConsole();
+                displayOpponents(player, enemy);
+                Enemy.botChoice(enemy, player,  battleLog);
+                playerBattleChoice(player, reader, enemy,  battleLog);
             }
 
         }
     }
 
-    public static void displayOpponets(Gladiator player, Gladiator enemy){
+    public static void displayBattleLog(ArrayList<String> battleLog){
+        String header = "----Battle Log----";
+        for (String log : battleLog){
+            header += "\n"+ log;
+        }
+        System.out.println(header);
+    }
+
+    public static void displayOpponents(Gladiator player, Gladiator enemy){
         String bar = "________________________________________\n";
         String singleFormat = "%-20s |  %s\n";
-        String fractionalFormat = "%s/%-20s |  %s/%-20s\n";
-        String finalFormat = bar+singleFormat+ fractionalFormat + bar;
-        System.out.printf(finalFormat, player.name, enemy.name, player.health,player.maxHealth,enemy.health ,enemy.maxHealth );
+        String healthFormat = "Health: %-4s/ %-6s |  Health: %-4s/ %s\n";
+        String manaFormat = "Mana: %-4s/ %-8s |  Mana: %-4s/ %s\n";
+
+        String finalFormat = bar + singleFormat + healthFormat + manaFormat + bar;
+        System.out.printf(
+                finalFormat,
+                player.name,
+                enemy.name,
+                player.health,
+                player.maxHealth,
+                enemy.health,
+                enemy.maxHealth,
+                player.mana,
+                player.maxMana,
+                enemy.mana,
+                enemy.maxMana
+                );
     }
 
 
-    public static void playerBattleChoice(Gladiator player, Scanner reader, Gladiator enemy){
+    public static void playerBattleChoice(Gladiator player, Scanner reader, Gladiator enemy, ArrayList<String> battleLog){
         while (true){
-            System.out.printf("[1] Attack [2] Cast Spell (%s) [3]Rest", player.manaCost);
+            System.out.printf("[1] Attack [2] Cast Spell (%s) [3] Rest ", player.manaCost);
             int choice = reader.nextInt();
             if (choice == 1){
                 player.attack(enemy);
+                battleLog.add(player.name + " Attacked" + enemy.name);
                 break;
             }else if (choice == 2){
-                if (player.mana > player.manaCost){
+                if (player.mana >= player.manaCost){
                     player.castSpell(enemy);
+                    battleLog.add(player.name + " Casted" + player.spellName);
                     break;
                 }
             }else if (choice == 3){
                 player.rest();
+                battleLog.add(player.name+ " Rested");
                 break;
             }
         }
     }
 
-
     public static String returnName(Scanner reader){
         while (true){
-            System.out.println("Enter Your Name");
+            System.out.print("Enter Your Name: ");
             String name = reader.nextLine().trim();
             if (name.length() <= 15){
                 return name;
@@ -81,15 +111,14 @@ public class Program {
                 System.out.println("\nName must be between 1-15 characters!\n");
             }
         }
-
     }
 
     public static Gladiator makeGladiator(String name, Scanner reader){
         System.out.println("Choose a Class\n[1]Warrior");
         while (true){
             System.out.print("Choice: ");
-            int classNum = reader.nextInt();
-            if (classNum == 1){
+            String classNum = reader.nextLine().trim();
+            if (classNum.equals("1")){
                 return new Warrior(name);
             }
         }
