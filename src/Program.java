fef5dcd3727ccd6affb912ac905cf6ecf52ac9ec
John <jmorgan@basecampcodingacademy.org>
2019-01-29 12:52:28 -0600

    Added gameLoader() to determine a new game or deserialize a Gladiator class for continuing gameplay.

--- src/Program.java
+++ src/Program.java
@@ -3,16 +3,37 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        String name = returnName(reader);
-        Gladiator player = makeGladiator(name, reader);
-        beginGame(player,reader);
 
-        System.out.println(player.name);
+        Scanner reader = new Scanner(System.in);
+        gameLoader(reader);
+
+    }
+
+    public static void gameLoader(Scanner reader){
+        while (true){
+            System.out.println("[1] New Game  [2] Load Game: ");
+            String response = reader.nextLine().trim();
+            if (response.equals("1") ){
+                String name = returnName(reader);
+                Gladiator player = makeGladiator(name, reader);
+                beginGame(player,reader);
+                break;
+            }else if (response.equals("2")){
+                System.out.println("Enter Save File Name: ");
+                String name = reader.nextLine().trim();
+                Gladiator player =  GladiatorSaver.load(name);
+                if (player != null){
+                    beginGame(player, reader);
+                }
+            }else{
+                System.out.print("Enter a valid option!");
+            }
+        }
+
     }
 
     public static void beginGame(Gladiator player, Scanner reader){
-        int round = 1;
+        int round = player.level;
         ArrayList<String> battleLog = new ArrayList<>();
 
         while (!player.isDead()) {
@@ -32,39 +53,49 @@ public class Program {
         boolean playerAttackFirst = Utils.flipCoin();
         if (playerAttackFirst){
             while (!player.isDead() && !enemy.isDead()) {
-                Utils.clearConsole();
                 displayOpponents(player, enemy);
                 playerBattleChoice(player, reader, enemy,  battleLog);
                 Enemy.botChoice(enemy, player,  battleLog);
+                displayBattleLog(battleLog);
             }
         }else {
             while (!player.isDead() && !enemy.isDead()) {
-                Utils.clearConsole();
                 displayOpponents(player, enemy);
                 Enemy.botChoice(enemy, player,  battleLog);
+                displayBattleLog(battleLog);
                 playerBattleChoice(player, reader, enemy,  battleLog);
+                displayBattleLog(battleLog);
             }
 
         }
     }
 
     public static void displayBattleLog(ArrayList<String> battleLog){
+        if (battleLog.size() > 2){
+            for (int i = 0; i < (battleLog.size() - 2); i++){
+                battleLog.remove(0);
+            }
+        }
         String header = "----Battle Log----";
         for (String log : battleLog){
             header += "\n"+ log;
         }
-        System.out.println(header);
+        System.out.println(header + "\n------------------\n");
+
+
     }
 
     public static void displayOpponents(Gladiator player, Gladiator enemy){
         String bar = "________________________________________\n";
+        String round="* Round %s *\n";
         String singleFormat = "%-20s |  %s\n";
         String healthFormat = "Health: %-4s/ %-6s |  Health: %-4s/ %s\n";
         String manaFormat = "Mana: %-4s/ %-8s |  Mana: %-4s/ %s\n";
 
-        String finalFormat = bar + singleFormat + healthFormat + manaFormat + bar;
+        String finalFormat = bar + round + singleFormat + healthFormat + manaFormat + bar;
         System.out.printf(
                 finalFormat,
+                player.level,
                 player.name,
                 enemy.name,
                 player.health,
@@ -81,22 +112,27 @@ public class Program {
 
     public static void playerBattleChoice(Gladiator player, Scanner reader, Gladiator enemy, ArrayList<String> battleLog){
         while (true){
-            System.out.printf("[1] Attack [2] Cast Spell (%s) [3] Rest ", player.manaCost);
+            System.out.printf("[1] Attack [2] Cast Spell (%s) [3] Rest [4] Save/Quit: ", player.manaCost);
             int choice = reader.nextInt();
             if (choice == 1){
                 player.attack(enemy);
-                battleLog.add(player.name + " Attacked" + enemy.name);
+                battleLog.add(player.name + " Attacked " + enemy.name);
                 break;
             }else if (choice == 2){
                 if (player.mana >= player.manaCost){
                     player.castSpell(enemy);
-                    battleLog.add(player.name + " Casted" + player.spellName);
+                    battleLog.add(player.name + " Casted " + player.spellName);
                     break;
                 }
             }else if (choice == 3){
                 player.rest();
                 battleLog.add(player.name+ " Rested");
                 break;
+            }else if (choice == 4){
+                boolean result =  GladiatorSaver.save(player);
+                if (result){
+                    System.exit(0);
+                }
             }
         }
     }
@@ -114,7 +150,7 @@ public class Program {
     }
 
     public static Gladiator makeGladiator(String name, Scanner reader){
-        System.out.println("Choose a Class\n[1]Warrior");
+        System.out.println("Choose a Class\n\t[1]Warrior\n");
         while (true){
             System.out.print("Choice: ");
             String classNum = reader.nextLine().trim();
